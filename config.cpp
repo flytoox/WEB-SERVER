2c125eb8969687729d1b2e64d9f6d1345e80c641
Omar Belaizi <80468732+flytoox@users.noreply.github.com>
2024-02-28 15:43:05 +0100

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -6,13 +6,15 @@
 /*   By: obelaizi <obelaizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 18:35:45 by obelaizi          #+#    #+#             */
-/*   Updated: 2024/02/01 16:13:10 by obelaizi         ###   ########.fr       */
+/*   Updated: 2024/02/03 20:15:12 by obelaizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Server.hpp"
-#include "configFile.hpp"
-#include "webserve.hpp"
+#include "includes/Server.hpp"
+#include "includes/configFile.hpp"
+#include "includes/webserve.hpp"
+#include <stdexcept>
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -21,19 +23,50 @@
 
 using namespace std;
 
-//TODO: Omar check index value if it has something; if not , set the value to index.html in the directives server block
-//TODO: Omar check location match if it's duplicated , error -> exit
-//TODO: Omar check the return if it gets Resonse StatusCode and next to it a URL; error -> exit
-//TODO: Omar don't remove /// in location -> DONNNNNN'T
+//Done: Omar check location if it's duplicated , error -> exit
+//Done: Omar check the return if it gets Resonse StatusCode and next to it a URL; error -> exit
+//Done: Omar don't remove /// in location -> DONNNNNN'T
 //DONE: Omar if autoindex is on , if autoindex is off remove autoindex key from scratch
+static vector<string> split(string s) {
+	stringstream ss(s);
+	vector<string> v;
+	string word;
+	while (ss >> word)
+		v.push_back(word);
+	return (v);
+}
 
-//TODO: you need to put the value of directives 
-//TODO :, then override them if they exist within that location block that you are parsing ; skip these three values :
-//TODO: server_name, host, listen 
+bool checkReturnOnLocation(vector<map<string, string> > &locationsBlock) {
+	for (size_t i = 0; i < locationsBlock.size(); i++) {
+		if (!locationsBlock[i].count("return"))
+			continue;
+		std::string returnStr = locationsBlock[i]["return"];
+		vector<string> v = split(returnStr);
+		if (v.size() != 2)
+			return (false);
+		int statusCode = stoi(v[0]);
+		if (statusCode < 100 || statusCode > 599)
+			return (false);
+	}
+	return (true);
+}
 
-//TODO: set [upload_store] [/Users/sizgunan/goinfre/upload] -> if [upload_enable] == [on]
+bool checkDuplicateLocation(vector<map<string, string> > &locationsBlock) {
+	set<string> locationSet;
+	for (size_t i = 0; i < locationsBlock.size(); i++) {
+		if (locationSet.count(locationsBlock[i]["location"])) {
+			return (true);
+		}
+		locationSet.insert(locationsBlock[i]["location"]);
+	}
+	return (false);
+}
 
-
+bool checkPortMaxMin(int port) {
+	if (port < 1024 || port > 65535)
+		return (false);
+	return (true);
+}
 
 void	GetDirectives(string &word, map<string, string> &directives, string &key) {
 	if (key == "")
@@ -113,18 +146,6 @@ void	duplicateServerBasedOnListen(vector<Server> &servers) {
 	}
 }
 
-void alo() {
-	return ;
-}
-
-static vector<string> split(string s) {
-	stringstream ss(s);
-	vector<string> v;
-	string word;
-	while (ss >> word)
-		v.push_back(word);
-	return (v);
-}
 
 vector<Server> parsingFile(string s) {
 	stack<string> st;
@@ -134,31 +155,34 @@ vector<Server> parsingFile(string s) {
 	vector<map<string, string> > locationsBlock;
 	string line;
 	ifstream file(s);
+	int lineNum = 0;
+	
 	if (file.is_open())
 	{
 		while (getline(file, line))
 		{
+			lineNum++;
 			vector<string> v = split(line);
 			if (v.size() == 0 || v[0][0] == '#')
 				continue;
 			if (v.size() == 1 && v[0].back() != '{' && v[0].back() != '}') {
-				throw runtime_error("Syntax error");
+				throw runtime_error("Syntax error on line " + to_string(lineNum));
 				return servers;
 			}
 			if (v.back().back() == '{') {
 				v.back().pop_back();
 				if (v.back() == "") v.pop_back();
 				if (v.empty()) throw runtime_error("Syntax error");
-				if (v.size() == 1) {
-					if (!st.empty()) throw runtime_error("Can't have a block inside a block");
+				if (v.size() == 1 && v[0] == "server") { // on server brackets
+					if (!st.empty()) throw runtime_error("Can't have a block inside a block on line " + to_string(lineNum));
 					st.push(v[0]);
 					locationsBlock.clear();
 					directives.clear();
 					continue;
 				}
 				if (v.size() != 2)
-					throw runtime_error("Error: too many arguments for " + v[0]);
-				st.push(v[0]);
+					throw runtime_error("Error: wrong number of arguments on " + v[0] + " line " + to_string(lineNum));
+				st.push(v[0]);// on location brackets
 				if (server.directives.empty())
 					server.directives = directives;
 				directives.clear();
@@ -166,7 +190,7 @@ vector<Server> parsingFile(string s) {
 				continue;
 			}
 			if (v.size() == 1 && v[0] == "}") {
-				if (st.empty()) throw runtime_error("Error: } without {");
+				if (st.empty()) throw runtime_error("Error: } without { on line " + to_string(lineNum));
 				if (st.top() == "server") {
 					server.locationsBlock = locationsBlock;
 					servers.push_back(server);
@@ -181,8 +205,12 @@ vector<Server> parsingFile(string s) {
 				st.pop();
 				continue;
 			}
+			if (st.empty()) throw runtime_error("Error: No block on line " + to_string(lineNum));
 			if (v[1].back() == ';') v[1].pop_back();
-			directives[v[0]] = v[1];
+			if (v[1].empty()) throw runtime_error("Error: No value on line " + to_string(lineNum));
+			if (v[0] == "cgi_bin" && directives.count(v[0]) && st.top() == "location") 
+				directives[v[0]] += '\n';
+			else directives[v[0]] = v[1];
 			for (size_t i = 2; i < v.size(); i++) {
 				if (v[i].back() == ';')
 					v[i].pop_back();
@@ -193,9 +221,26 @@ vector<Server> parsingFile(string s) {
 		file.close();
 	}
 	if (!st.empty())
-		return (cout << "FUCK U DONT PLAY WITH ME\n", servers);
+		throw runtime_error("Error: { without } on line " + to_string(lineNum));
+	
+	for (size_t i = 0; i < servers.size(); i++) {
+		int port;
+		try {
+			for (char &c : servers[i].directives["listen"])
+				if (!isdigit(c)) throw runtime_error("Error: Invalid port number on server Num " + to_string(i+1));
+			port = stoi(servers[i].directives["listen"]);
+		} catch (exception &e) {
+			throw runtime_error("Error: Invalid port number on server Num " + to_string(i+1));
+		}
+		if (!checkPortMaxMin(port))
+			throw runtime_error("Error: Port number should be between 1024 and 65535 on server Num " + to_string(i+1));
+		if (checkDuplicateLocation(servers[i].locationsBlock))
+			throw runtime_error("There is a duplicate Location on server Num " + to_string(i+1));
+		if (!checkReturnOnLocation(servers[i].locationsBlock))
+			throw runtime_error("Error: Invalid return on server Num " + to_string(i+1));
+	}
+
 	duplicateServerBasedOnListen(servers);
-	alo();
 	set<std::pair<string, string>> Check;
 	for (size_t i = 0; i < servers.size(); i++)
 	{
@@ -214,7 +259,6 @@ vector<Server> parsingFile(string s) {
 		}
 		int add = 1;
 		setsockopt(servers[i].socketD, SOL_SOCKET, SO_REUSEADDR, &add, sizeof(add));
-
 		servers[i].setSocketDescriptor(servers[i].socketD);
 		//* SAVE HISTORY ( HOST & PORT )
 		if (!Check.count({servers[i].directives["listen"], servers[i].directives["host"]})) {
@@ -222,18 +266,17 @@ vector<Server> parsingFile(string s) {
 			servers[i].listenToIncomingConxs();
 			Check.insert({servers[i].directives["listen"], servers[i].directives["host"]});
 		}
-		// servers.erase(servers.begin()+1, servers.begin() + servers.size());
-		// break ;
+		servers.erase(servers.begin()+1, servers.begin() + servers.size());
+		break ;
 	}
 	// set index.html if index is empty, and remove autoindex if it's off
 	for (size_t i = 0; i < servers.size(); i++) {
-		for (size_t j = 0; j < servers[i].locationsBlock.size(); j++) {
-			// if (!servers[i].locationsBlock[j].count("index"))
-			// 	servers[i].locationsBlock[j]["index"] = "index.html";
-			if (servers[i].locationsBlock[j].count("autoindex") && servers[i].locationsBlock[j]["autoindex"] == "off")
-				servers[i].locationsBlock[j].erase("autoindex");
+		for (size_t j = 0; j < servers[i].directives.size(); j++) {
+			if (!servers[i].directives.count("index"))
+				servers[i].directives["index"] = "index.html";
+			if (servers[i].directives.count("autoindex") && servers[i].directives["autoindex"] == "off")
+				servers[i].directives.erase("autoindex");
 		}
 	}
 	return (servers);
 }
-
